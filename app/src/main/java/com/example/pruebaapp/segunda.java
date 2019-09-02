598456e8b9797d2ee0b2d6a0830b5176ca519514
samuel <samuelmauricio@upeu.edu.pe>
2019-09-02 14:19:58 -0500

    Initial commit

---
+++ app/src/main/java/com/example/pruebaapp/segunda.java
@@ -0,0 +1,14 @@
+package com.example.pruebaapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class segunda extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_segunda);
+    }
+}
